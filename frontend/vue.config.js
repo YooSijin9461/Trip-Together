5d8fe5d42d9e168daa493999502d592ec652a4bc
김선홍 <ksh7628@naver.com>
2021-08-08 16:11:13 +0900

    no message

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,19 +8,19 @@ module.exports = {
     proxy: {
       '/api/v1': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io/'
+        target: 'https://i5d201.p.ssafy.io:8443/'
       },
       '/webjars': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io/'
+        target: 'https://i5d201.p.ssafy.io:8443/'
       },
       '/group-call': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io/'
+        target: 'https://i5d201.p.ssafy.io:8443/'
       },
       '/upload': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io/'
+        target: 'https://i5d201.p.ssafy.io:8443/'
       },
     },
     historyApiFallback: true,
