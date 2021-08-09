812d70f651fbf26b29a241533c41d5a1557c9b34
seokhwandan <dansh87@gmail.com>
2021-08-09 20:33:18 +0900

    no message

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,19 +8,27 @@ module.exports = {
     proxy: {
       '/api/v1': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io:8443/'
+        target: 'https://i5d201.p.ssafy.io:8443/',
+        ssl: 'keystore/ssafy.p12',
+        wss: true,
       },
       '/webjars': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io:8443/'
+        target: 'https://i5d201.p.ssafy.io:8443/',
+        ssl: 'keystore/ssafy.p12',
+        wss: true,
       },
       '/group-call': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io:8443/'
+        target: 'https://i5d201.p.ssafy.io:8443/',
+        ssl: 'keystore/ssafy.p12',
+        wss: true,
       },
       '/upload': {
         // target: 'https://localhost:8443/'
-        target: 'https://i5d201.p.ssafy.io:8443/'
+        target: 'https://i5d201.p.ssafy.io:8443/',
+        ssl: 'keystore/ssafy.p12',
+        wss: true,
       },
     },
     historyApiFallback: true,
