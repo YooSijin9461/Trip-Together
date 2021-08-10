8c1a7638fbf6938a555ff8e999df321405220393
seokhwandan <dansh87@gmail.com>
2021-08-10 15:56:49 +0900

    comment url 수정

--- backend/src/main/java/com/ssafy/api/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/api/controller/CommentController.java
@@ -43,7 +43,7 @@ import io.swagger.annotations.ApiResponses;
  */
 @Api(value = "댓글 API", tags = {"Comment"})
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/comments")
 @CrossOrigin("*")
 public class CommentController {
 	@Autowired
@@ -62,7 +62,7 @@ public class CommentController {
 		
 	}
 	
-	@GetMapping("/{boardNo}/comments")
+	@GetMapping("/{boardNo}")
 	@ApiOperation(value = "댓글 목록", notes = "게시글 번호에 해당하는 댓글 목록 반환")
 	@ApiResponses({
 		@ApiResponse(code = 200, message = "성공"),
@@ -72,7 +72,7 @@ public class CommentController {
 		return new ResponseEntity<List<Comments>>(commentService.selectComment(boardNo), HttpStatus.OK);
 	}
 	
-	@PostMapping("/comments/{commentNo}")
+	@PostMapping("/{commentNo}")
 	@ApiOperation(value = "댓글 상세 조회", notes = "<strong>댓글 번호</strong>를 통해 댓글 상세정보 조회")
 	@ApiResponses({
 		@ApiResponse(code = 200, message = "성공"),
@@ -82,7 +82,7 @@ public class CommentController {
 		return new ResponseEntity<>(commentService.getInfoByCommentNo(commentNo), HttpStatus.OK);
 	}
 	
-	@PatchMapping("/comments/{commentNo}")
+	@PatchMapping("/{commentNo}")
 	@ApiOperation(value = "댓글 수정")
 	@ApiResponses({
 	        @ApiResponse(code = 200, message = "성공"),
@@ -96,7 +96,7 @@ public class CommentController {
 		return new ResponseEntity<>(comment, HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/comments/{commentNo}")
+	@DeleteMapping("/{commentNo}")
 	@ApiOperation(value = "댓글 삭제")
 	@ApiResponses({
 	        @ApiResponse(code = 200, message = "성공"),
