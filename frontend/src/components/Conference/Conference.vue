3ea88012779e0eb723927425036ac5e6a7a972f4
김선홍 <ksh7628@naver.com>
2021-08-10 09:31:39 +0900

    no message

--- frontend/src/components/Conference/Conference.vue
+++ frontend/src/components/Conference/Conference.vue
@@ -58,7 +58,7 @@ export default {
     // webRTC 기능 
 
     const kurentoUtils = require('kurento-utils')
-    const socket = new WebSocket('wss://i5d201.p.ssafy.io:8443/groupcall')
+    var socket = new WebSocket('wss://i5d201.p.ssafy.io:8443/groupcall')
     const PARTICIPANT_MAIN_CLASS = 'participant main';
     const PARTICIPANT_CLASS = 'participant';
     const participants = {}
