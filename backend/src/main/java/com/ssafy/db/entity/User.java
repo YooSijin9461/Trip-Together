33abd4a729e985847586d376589c5dd9178fcae9
SEO GWEONWOO <rnjsdn2481@naver.com>
2021-08-11 20:57:36 +0900

    entity -> User getter/setter 직접 추가

--- backend/src/main/java/com/ssafy/db/entity/User.java
+++ backend/src/main/java/com/ssafy/db/entity/User.java
@@ -21,13 +21,125 @@ import org.springframework.web.multipart.MultipartFile;
  * 유저 모델 정의.
  */
 @Entity
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class User extends BaseEntity{
 	@Id
 	String userId;
 	
-    String userName;
+    public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getMbti() {
+		return mbti;
+	}
+
+	public void setMbti(String mbti) {
+		this.mbti = mbti;
+	}
+
+	public double getAvgScore() {
+		return avgScore;
+	}
+
+	public void setAvgScore(double avgScore) {
+		this.avgScore = avgScore;
+	}
+
+	public boolean isGuide() {
+		return isGuide;
+	}
+
+	public void setGuide(boolean isGuide) {
+		this.isGuide = isGuide;
+	}
+
+	public Integer getConferenceRoomNo() {
+		return conferenceRoomNo;
+	}
+
+	public void setConferenceRoomNo(Integer conferenceRoomNo) {
+		this.conferenceRoomNo = conferenceRoomNo;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getOrgImg() {
+		return orgImg;
+	}
+
+	public void setOrgImg(String orgImg) {
+		this.orgImg = orgImg;
+	}
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	String userName;
     char gender;
     String phoneNum;
     String email;
