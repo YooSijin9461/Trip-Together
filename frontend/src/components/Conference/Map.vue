7243d9cfca5ec93547c080e5ce6d57a330c9f97e
DESKTOP-KVCQHCD\multicampus <youjehag@naver.com>
2021-08-13 17:56:54 +0900

    map 수정

--- frontend/src/components/Conference/Map.vue
+++ frontend/src/components/Conference/Map.vue
@@ -5,51 +5,51 @@
 </template>
 
 <script>
-import { reactive, onMounted } from 'vue'
+import { reactive } from 'vue'
 
 export default {
   setup() {
     const state = reactive ({
 
     })
-    const google = window.google
-    var map
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37, lng: 126},
-        zoom: 7,
-        styles: [{
-          featureType: 'poi',
-          stylers: [{ visibility: 'on' }]  // Turn off POI.
-        },
-        {
-          featureType: 'transit.station',
-          stylers: [{ visibility: 'on' }]  // Turn off bus, train stations etc.
-        }],
-        disableDoubleClickZoom: true,
-        streetViewControl: true,
-      });
+    // const google = window.google
+    // var map
+    // function initMap() {
+    //   map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {lat: 37, lng: 126},
+    //     zoom: 7,
+    //     styles: [{
+    //       featureType: 'poi',
+    //       stylers: [{ visibility: 'on' }]  // Turn off POI.
+    //     },
+    //     {
+    //       featureType: 'transit.station',
+    //       stylers: [{ visibility: 'on' }]  // Turn off bus, train stations etc.
+    //     }],
+    //     disableDoubleClickZoom: true,
+    //     streetViewControl: true,
+    //   });
 
-      map.addListener('click', function(e) {
-        var marker = new google.maps.Marker({
-          position: e.latLng,
-          map: map,
-        });
-        marker.addListener('dblclick', () => {
-          marker.setMap(null)
-        })
-        marker.addListener('click', function() {
-          console.log('클릭')
-          map.center = marker.position
-          map.zoom = 12
-        })
-      });
-    }
-    onMounted (() => {
-      initMap()
-    })
+    //   map.addListener('click', function(e) {
+    //     var marker = new google.maps.Marker({
+    //       position: e.latLng,
+    //       map: map,
+    //     });
+    //     marker.addListener('dblclick', () => {
+    //       marker.setMap(null)
+    //     })
+    //     marker.addListener('click', function() {
+    //       console.log('클릭')
+    //       map.center = marker.position
+    //       map.zoom = 12
+    //     })
+    //   });
+    // }
+    // onMounted (() => {
+    //   initMap()
+    // })
 
-    return { state, onMounted }
+    return { state }
   },
 }
 </script>
